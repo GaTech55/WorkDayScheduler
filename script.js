9227a2f7741607702575d5b63dfe9f8f6de60ccf
David Brown <gatech55@gmail.com>
2020-10-05 03:08:56 -0400

    added test variables to get a consistent look due to the time that i created this wow

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ $(document).ready(function () {
   console.log("Hello World");
   var currentDay = moment().format("dddd, MMMM Do");
   var currentTime = moment().format("HH");
+  var testTime = "13";
   console.log(currentTime);
-  var timeValues = [
-    "9AM",
-    "10AM",
-    "11AM",
-    "12PM",
-    "1PM",
-    "2PM",
-    "3PM",
-    "4PM",
-    "5PM",
-  ];
+  //   var timeValues = [
+  //     "9AM",
+  //     "10AM",
+  //     "11AM",
+  //     "12PM",
+  //     "1PM",
+  //     "2PM",
+  //     "3PM",
+  //     "4PM",
+  //     "5PM",
+  //   ];
   var timeValuesObj = [
     {
       display: "2AM",
@@ -59,14 +60,39 @@ $(document).ready(function () {
 
   for (var i = 0; i < timeValuesObj.length; i++) {
     var hour = $("<div>");
+    var timeBlock = $("<div>");
+    var saveBtn = $("<button>");
     hour.addClass("hour");
+    timeBlock.addClass("time-block");
+    saveBtn.addClass("saveBtn saveBtn i:hover");
     hour.attr("timeAttribute", timeValuesObj[i].value);
+    timeBlock.attr("blockAttribute", timeValuesObj[i].value);
+    saveBtn.attr("buttonAttribute", timeValuesObj[i].value);
     hour.text(timeValuesObj[i].display);
+    timeBlock.text(timeValuesObj[i].display);
+    saveBtn.text(timeValuesObj[i].display);
     $("#hour").append(hour);
+    $("#timeBlock").append(timeBlock);
+    $("#saveBtn").append(saveBtn);
 
-    if (currentTime === timeValuesObj[i].value) {
+    //Adding a test block of code for time
+    if (parseInt(testTime) === parseInt(timeValuesObj[i].value)) {
       console.log(timeValuesObj[i].display);
+      timeBlock.addClass("present");
+    } else if (parseInt(testTime) > parseInt(timeValuesObj[i].value)) {
+      timeBlock.addClass("past");
+    } else {
+      timeBlock.addClass("future");
     }
+    //Below will be the final block of code
+    // if (parseInt(currentTime) === parseInt(timeValuesObj[i].value)) {
+    //   console.log(timeValuesObj[i].display);
+    //   hour.addClass("present");
+    // } else if (parseInt(currentTime) < parseInt(timeValuesObj[i].value)) {
+    //   hour.addClass("past");
+    // } else {
+    //   hour.addClass("future");
+    // }
   }
 
   $("#currentDay").append(currentDay);
